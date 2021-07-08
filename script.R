3639376fba775fd9b5f0ebda97b222e5a889e2b7
wesley4546 <wesley.gardiner4546@gmail.com>
2021-07-08 17:51:15 -0400

    starting it in a script

---
+++ script.R
@@ -0,0 +1,22 @@
+
+# Package installing/loading ----------------------------------------------
+
+# Below are the packages used and a brief description of their purpose
+
+# `dplyr` - Data wrangling with ease!
+# `tidyr`- Functions to help in the pursuit of "tidy data"
+# `ggplot2` - The bee's knees of data visualization
+# `readr` - A better way to read in data
+# `modeest` - A solution to missing mode funcationality within R
+
+
+install.packages(c("dplyr","tidyr","ggplot2","readr","modeest"))
+
+
+# Level 1 -----------------------------------------------------------------
+
+# Create the total superficial judgment summary score 
+# (i.e., sum judge_pic, judge_outfit, judge_job, judge_born, judge_live,
+# judge_car, judge_uni, judge_diet, judge_drink, judge_teeth, judge_posts,
+# judge_grammar, judge_sparetime)
+
